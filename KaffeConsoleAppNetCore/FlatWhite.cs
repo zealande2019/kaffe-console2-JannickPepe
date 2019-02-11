2e7a606b7a32090e0f2de17bbd6221a460c5faf4
jannick <jannickpedersen@live.dk>
2019-02-11 10:43:06 +0100

    Har lavet FlatWhite, lavet nevarvnings egenskaber til obejkter og derved kunne printe dem ud sammen med Flatwhite

---
+++ KaffeConsoleAppNetCore/FlatWhite.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kaffe
+{
+    public class FlatWhite : Kaffe, Imælk
+    {
+        /// <summary>
+        /// Hvor meget mælk skal bruges til Latte
+        /// </summary>
+        /// <returns></returns>
+        public int MlMælk()
+        {
+            return 160; 
+        }
+
+        public override int Pris()
+        {
+            return 45;
+        }
+
+        public override string Styrke()
+        {
+            return base.Styrke();
+        }
+
+
+    }
+}
--- KaffeConsoleAppNetCore/Program.cs
+++ KaffeConsoleAppNetCore/Program.cs
@@ -9,9 +9,16 @@ namespace KaffeConsoleAppNetCore
         {
             Console.WriteLine("Hello World!");
 
-            
+            Cortado kaffe1 = new Cortado();
+            Latte kaffe2 = new Latte();
+            SortKaffe kaffe3 = new SortKaffe();
+            FlatWhite kaffe4 = new FlatWhite();
 
-           
+            //Ellers kan jeg også lave navne for kafferne også nedarve navnet på kafferne mht navnet i console.writeline
+
+            Console.WriteLine("Cortado " + kaffe1.Pris() + "kr-; " + kaffe1.Styrke());
+            Console.WriteLine("FlatWhite " + kaffe4.Pris() + "kr-;" + kaffe4.Styrke());
+            Console.ReadLine();
 
         }
     }
